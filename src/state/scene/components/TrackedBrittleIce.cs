79ae2965846d323e0a1187b0683caaf3ef6a7b69
Kaden5480 <kaden5480@protonmail.com>
2025-03-09 22:05:08 +0000

    Fix resource leak

--- src/state/scene/components/TrackedBrittleIce.cs
+++ src/state/scene/components/TrackedBrittleIce.cs
@@ -157,6 +157,7 @@ namespace FastReset.State {
                 iceCracksRenderers[0].material
             );
 
+            temporaryPositions.Clear();
             foreach (Rigidbody rb in myRBs) {
                 temporaryPositions.Add(PositionFix.RealToOffset(rb.transform.position));
                 temporaryRotations.Add(rb.transform.rotation);
